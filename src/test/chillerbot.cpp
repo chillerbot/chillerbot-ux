3cd9564be7e2a75e9b85f67233e5d81eba126725
ChillerDragon <chillerdragon@gmail.com>
2026-06-27 09:43:58 +0200

    ux: move langparser tests to submodule lib

--- src/test/chillerbot.cpp
+++ src/test/chillerbot.cpp
@@ -65,42 +65,4 @@
 // 	// EXPECT_STREQ(aBuf, "|*KoG*| GER #3 - Main Gores [kog.tw]                        ");
 // }
 
-TEST(ChillerBot, LangParserAskToAsk)
-{
-	bool AskToAsk = LangParser::IsAskToAsk("Can I ask you a question?");
-	EXPECT_EQ(AskToAsk, true);
-	AskToAsk = LangParser::IsAskToAsk("Kann ich dich was fragen?");
-	EXPECT_EQ(AskToAsk, true);
-}
 
-TEST(ChillerBot, LangParserAskToAskStatement)
-{
-	bool AskToAsk = LangParser::IsAskToAsk("I have a question");
-	EXPECT_EQ(AskToAsk, true);
-	AskToAsk = LangParser::IsAskToAsk("Ich hab eine frage");
-	EXPECT_EQ(AskToAsk, true);
-}
-
-TEST(ChillerBot, LangParserWantedToAskFalsePositive)
-{
-	bool AskToAsk = LangParser::IsAskToAsk("I wanted To ask you something, but then I did not care");
-	EXPECT_EQ(AskToAsk, false);
-
-	AskToAsk = LangParser::IsAskToAsk("I just wanted to ask how are you");
-	EXPECT_EQ(AskToAsk, false);
-
-	AskToAsk = LangParser::IsAskToAsk("I just wanted to ask");
-	EXPECT_EQ(AskToAsk, false);
-}
-
-TEST(ChillerBot, LangParserWantedToAskFalsePositiveGer)
-{
-	bool AskToAsk = LangParser::IsAskToAsk("ich wollte dich fragen, doch dann wars mir egal");
-	EXPECT_EQ(AskToAsk, false);
-
-	AskToAsk = LangParser::IsAskToAsk("Ich wollt dich nur fragen wie es dir geht");
-	EXPECT_EQ(AskToAsk, false);
-
-	AskToAsk = LangParser::IsAskToAsk("das wollte ich dich schon immer mal fragen");
-	EXPECT_EQ(AskToAsk, false);
-}
